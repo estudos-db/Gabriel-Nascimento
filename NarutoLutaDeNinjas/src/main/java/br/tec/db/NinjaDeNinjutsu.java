a077d7ca304fe58cbe090bac420790cad81d1dcf
GabrielNS <gabriel.nascimento33@hotmail.com>
2023-04-17 22:12:35 -0300

    narutoLutaDeNinja-solucao

---
+++ NarutoLutaDeNinjas/src/main/java/br/tec/db/NinjaDeNinjutsu.java
@@ -0,0 +1,18 @@
+package br.tec.db;
+
+public class NinjaDeNinjutsu extends Personagem implements Ninja{
+
+    public NinjaDeNinjutsu(String nome, int idade, String aldeia, int chakra){
+        super(nome, idade, aldeia,chakra);
+    }
+
+    @Override
+    public void usarJutsu() {
+        System.out.println("Usando um jutsu de Ninjutsu");
+    }
+
+    @Override
+    public void desviar() {
+        System.out.println("Desviando de um ataque usando Ninjutsu");
+    }
+}
---
+++ NarutoLutaDeNinjas/src/test/java/NinjasTest.java
@@ -0,0 +1,43 @@
+import br.tec.db.NinjaDeGenjutsu;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class NinjasTest {
+
+    NinjaDeGenjutsu ninjaGenjutsu;
+    private final ByteArrayOutputStream stringMostrada = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void inicializa(){
+        ninjaGenjutsu = new NinjaDeGenjutsu("Itachi", 18, "Folha", 150);
+        System.setOut(new PrintStream(stringMostrada));
+    }
+
+    @Test
+    void usaOMetodoUsarJutsuEMostraMensagem(){
+        ninjaGenjutsu.usarJutsu();
+        Assertions.assertEquals("Usando um jutsu de Genjutsu\r\n", stringMostrada.toString());
+    }
+
+    @Test
+    void usaOMetodoDesviarEMostraMensagem(){
+        ninjaGenjutsu.desviar();
+        Assertions.assertEquals("Desviando de um ataque usando Genjutsu\r\n", stringMostrada.toString());
+    }
+
+    @Test
+    void aumentaChakraDoNinja(){
+        ninjaGenjutsu.aumentaChakra(50);
+        Assertions.assertEquals(200, ninjaGenjutsu.getChakra());
+    }
+
+    @Test
+    void adicionandoJutsusNoArrayDeJutsus(){
+        ninjaGenjutsu.adicionaJutsu("Tsukuyomi");
+        Assertions.assertEquals(1, ninjaGenjutsu.getJutsus().length);
+    }
+}
